4b8a4d7be74f66ad3188420bace78c2b37cf1b15
Benedikt Lang <github@benediktlang.de>
2015-08-11 23:42:45 +0200

    feat: import storage directory

---
+++ photowall.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"flag"
+	"github.com/blang/photowall/wall"
+	"github.com/blang/photowall/web"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"time"
+)
+
+var listen = flag.String("listen", ":8000", "Listen addr")
+var storeDir = flag.String("storedir", "./imgs", "Storage directory")
+var argAllowedExts = flag.String("allow", "png,jpg", "Allowed file extensions")
+var argImgWidth = flag.Uint("img_width", 1920, "Resize bigger images to this width")
+var argImgHeight = flag.Uint("img_height", 1080, "Resize bigger images to this height")
+var argMaxFileSize = flag.Int("filesize_max", 10, "Maximum upload filesize in MB")
+
+func baseDir() string {
+	_, file, _, _ := runtime.Caller(0)
+	return filepath.Dir(file)
+}
+
+func main() {
+	flag.Parse()
+	pwall := wall.Create()
+	pwall.SetProcessors([]wall.Processor{
+		wall.Importer(),
+	})
+	restoreFromDirectory(pwall, filepath.Join(baseDir(), *storeDir))
+	pwall.SetProcessors([]wall.Processor{
+		wall.NewResizer(*argImgWidth, *argImgHeight),
+		wall.NewStore(filepath.Join(baseDir(), *storeDir)),
+	})
+
+	server := web.NewServer(pwall, filepath.Join(baseDir(), "/static"), filepath.Join(baseDir(), *storeDir), int64(*argMaxFileSize)*1024*1025, *argAllowedExts)
+	log.Fatal(server.Run(*listen))
+}
+
+func restoreFromDirectory(wall wall.Photowall, path string) {
+	log.Printf("Restore store from directory: %s\n", path)
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Printf("Error reading directory: %s", err)
+		return
+	}
+	for _, f := range files {
+		log.Printf("File: %s", f.Name())
+		if strings.ToLower(filepath.Ext(f.Name())) == ".jpg" {
+			fullpath := filepath.Join(path, f.Name())
+			log.Printf("Added file: %s", fullpath)
+			wall.AddPhotoFromFile(fullpath, time.Now())
+		}
+	}
+}
